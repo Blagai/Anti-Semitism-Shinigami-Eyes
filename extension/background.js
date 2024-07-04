93d0cf187489a6929d4a7802947ec4a6204e9ba2
No <AyEmREL@protonmail.com>
2024-07-05 00:39:16 +0300

    Add shit
    Mark as anti-semitic in context menu functionality (only locally saved for now). Tidy shit up.

--- extension/background.js
+++ extension/background.js
@@ -1,13 +1,7 @@
 chrome.runtime.onInstalled.addListener(() => {
-	fetch(chrome.runtime.getURL('data/anti.txt'))
-	.then(response => response.text())
-	.then(text => {
-		const AntiSem = text.split('\n').map(domain => domain.trim()).filter(domain => domain.trim() !== '');
-		chrome.storage.local.set({ AntiSem }, () => {
-			console.log('Logged domains saved:', AntiSem);
-		});
-	})
-	.catch(error => console.error('Error fetching domains:', error));
+	// Fetch anti-semitic data
+	FetchAntiData();
+	CreateContextMenu();
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
@@ -18,3 +12,45 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 		return true; // Keeps the message channel open for sendResponse
 	}
 });
+
+// Context menu click handling
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+	if (info.menuItemId === "WriteAnti") {
+		WriteToAnti(info.linkUrl);
+	}
+});
+
+// Function to fetch data from the anti-semitic data file
+function FetchAntiData() {
+	fetch(chrome.runtime.getURL('data/anti.txt'))
+	.then(response => response.text())
+	.then(text => {
+		const AntiSem = text.split('\n').map(domain => domain.trim()).filter(domain => domain.trim() !== '');
+		chrome.storage.local.set({ AntiSem }, () => {
+			console.log('Logged domains saved:', AntiSem);
+		});
+	})
+	.catch(error => console.error('Error fetching domains:', error));
+}
+
+// Function to create context menu with required buttons
+function CreateContextMenu() {
+	chrome.contextMenus.create({
+		id: "WriteAnti",
+		title: "Mark as anti-semitic",
+		contexts: ["link"]
+	});
+}
+
+// Function to write marked link to anti-semitic data file
+function WriteToAnti(linkUrl) {
+	chrome.storage.local.get('AntiSem', data => {
+		const AntiSem = data.AntiSem || [];
+		if (!AntiSem.includes(linkUrl)) {
+			AntiSem.push(linkUrl);
+			chrome.storage.local.set({ AntiSem }, () => {
+				console.log('updated domains saved:', AntiSem);
+			});
+		}
+	});
+}
